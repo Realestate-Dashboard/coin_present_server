6e37e26c504f33cdce476ad1bd5e3c2e0539f8c1
sky81219 <=>
2023-01-09 18:37:34 +0900

    준비

--- present_backend/app/main.py
+++ present_backend/app/main.py
@@ -1,5 +1,22 @@
+import os
+import sys
+import json
+from typing import Final, List
+
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))))
+
+
+from kafka_distribute.consumer import consumer_optional
 from fastapi import FastAPI
 from typing import Optional
+
+
+
+COIN_PRECENT_PRICE: Final[str] = "coin_price"
+bootstrap_server: List[str] = ["kafka1:19091", "kafka2:29092", "kafka3:39093"]
+price = consumer_optional(topic=COIN_PRECENT_PRICE, bootstrap_server=bootstrap_server)
+
 
 app = FastAPI()
 
