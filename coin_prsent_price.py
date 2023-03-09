50339d65a378e9f64ccac77a0e967a723e1534d0
sky81219 <=>
2023-03-09 22:26:38 +0900

    소켓연결

--- coin_prsent_price.py
+++ coin_prsent_price.py
@@ -1,3 +1,39 @@
+import sys
+from pathlib import Path
+
+# 현재 파일의 경로
+file_path = Path(__file__).resolve()
+
+# 상위 경로 backend
+parent_path = file_path.parent
+
+# 상위 경로 backed_pre -> sys 경로 추가
+grandparent_path = parent_path.parent
+backend__ = f"{str(grandparent_path)}/backend/backend_pre"
+sys.path.append(str(parent_path))
+sys.path.append(str(grandparent_path))
+sys.path.append(backend__)
+
+
+import os
+import django
+from channels.layers import get_channel_layer
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.backend_pre.config.settings.dev')
+django.setup()
+
+channel_layer = get_channel_layer()
+ 
+async def send_data_to_channels_layer(data):
+    await channel_layer.group_send(
+        'stream_group', 
+        {
+            'type': 'send_stream_data', 
+            'data': data
+        }
+    )
+
 from pyspark.conf import SparkConf
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import udf
@@ -5,20 +41,24 @@ from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import (
     StructType, StructField, DoubleType, StringType
 )
+import asyncio
+from udf_util import test_straming_preprocessing
 
-from ss import test_straming_preprocessing
 
-
-schema = StructType([
-    StructField("name", StringType()),
-    StructField("timestamp", DoubleType()),
-    StructField("data", StructType([
-        StructField("opening_price", DoubleType()),
-        StructField("closing_price", DoubleType()),
-        StructField("max_price", DoubleType()),
-        StructField("min_price", DoubleType())
+schema: StringType = StructType([
+    StructField("average", StructType([
+        StructField("name", StringType()),
+        StructField("timestamp", DoubleType()),
+        StructField("data", StructType([
+            StructField("opening_price", DoubleType()),
+            StructField("closing_price", DoubleType()),
+            StructField("max_price", DoubleType()),
+            StructField("min_price", DoubleType())
+        ]))
     ]))
 ])
+    
+
 average_udf = udf(test_straming_preprocessing, schema)
 
 
@@ -61,9 +101,7 @@ spark_conf = SparkConf().setAppName("MyApp")\
     .setMaster("local[*]")\
     .set("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.0") \
 
-
-spark = SparkSession.builder.config(conf=spark_conf).getOrCreate()
-
+spark = SparkSession.builder.config(conf=spark_conf).getOrCreate()    
 
 # 스트리밍 데이터 생성
 stream_df = spark.readStream.format("kafka") \
@@ -87,9 +125,8 @@ query = result_df.writeStream.format("kafka") \
     .option("kafka.bootstrap.servers", "kafka1:19092,kafka2:29092,kafka3:39092") \
     .option("topic", "trade_bitcoin_total") \
     .option("checkpointLocation", ".checkpoint")\
-    .foreach(print)\
+    .foreach(lambda x: asyncio.ensure_future(send_data_to_channels_layer(x)))\
     .start()
-
-
+        
 query.awaitTermination()
 
--- udf_util.py
+++ udf_util.py
@@ -1,3 +1,17 @@
+import sys
+from pathlib import Path
+
+# 현재 파일의 경로
+file_path = Path(__file__).resolve()
+
+# 상위 경로 backend
+parent_path = file_path.parent
+
+# 상위 경로 backed_pre -> sys 경로 추가
+grandparent_path = parent_path.parent
+sys.path.append(str(parent_path))
+sys.path.append(str(grandparent_path))
+
 import numpy as np
 from typing import Any, Tuple, List, Dict
 from backend.coin_price.schema.schema import utc_time
@@ -6,7 +20,7 @@ from backend.coin_price.schema.schema import utc_time
 def test_straming_preprocessing(*data: Tuple[Any]) -> Dict:
   roww: List = [d for d in data]  
   value: List[Tuple] = list(zip(*roww))
-  average: Any = np.mean(value, axis=1)
+  average: Any = np.mean(value, axis=1).tolist()
   data: Dict = {
       "average": {
         "name": "BTC",
